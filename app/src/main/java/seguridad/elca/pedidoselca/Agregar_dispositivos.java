b38115bfc2a5a2174f4d28224fe34022c492b11e
nicolasmd1985 <nicolasmelectronic@gmail.com>
2016-08-01 10:09:07 -0300

    dispositivos

---
+++ app/src/main/java/seguridad/elca/pedidoselca/Agregar_dispositivos.java
@@ -0,0 +1,13 @@
+package seguridad.elca.pedidoselca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agregar_dispositivos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_dispositivos);
+    }
+}
